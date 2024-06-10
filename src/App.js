cbe254a4bb05d9dff0b8a63bf83d8f62513a5484
Valery <nellir@mail.ru>
2024-06-10 11:25:42 +0300

    11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import TodoList from './Todo/TodoList'
 
-function App() {
+function App() {  
+  const todos =[
+    { id: 1, completed: false, title: 'Купить хлеб' },
+    { id: 2, completed: false, title: 'Купить молоко' },
+    { id: 3, completed: false, title: 'Купить кефир' }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='wrapper'>
+    <h1> React tutorial</h1>
+
+    <TodoList todos={todos} />
+  </div>
+  )
 }
 
 export default App;
